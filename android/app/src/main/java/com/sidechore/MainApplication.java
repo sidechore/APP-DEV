588a6c16375a52ee023f725ff30dbc11ac8bcbe8
dev <master.world029@gmail.com>
2019-07-12 11:13:48 +1000

    Today work

--- android/app/src/main/java/com/sidechore/MainApplication.java
+++ android/app/src/main/java/com/sidechore/MainApplication.java
@@ -36,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
         protected List<ReactPackage> getPackages() {
             return Arrays.<ReactPackage>asList(
                     new MainReactPackage(),
+            new FBSDKPackage(),
                     new PreferencePackage(),
                     new MapsPackage(),
                     new DatePickerPackage(),
